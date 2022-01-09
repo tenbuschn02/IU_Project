1092ef51dadfb7ee0419e57c7b0c3efbe431fae2
tenbuschn02 <niclas.ten@web.de>
2022-01-09 19:33:12 +0100

    Basic layout completed

--- website/static/index.js
+++ website/static/index.js
@@ -6,3 +6,22 @@ function deleteNote(noteId) {
     window.location.href = "/";
   });
 }
+
+function showMenu() {
+  console.log('test')
+  const mobileBtn = document.getElementById('mobile-cta')
+        nav = document.querySelector('nav');
+        movileBtnExit = document.getElementById('mobile-exit');
+
+      mobileBtn.addEventListener('click', () => {
+        nav.classList.add('menu-btn');
+      })
+
+      movileBtnExit.addEventListener('click', () => {
+        nav.classList.remove('menu-btn');
+      })
+}
+
+function closeMessage(flash) {
+  $(flash).parent().remove()
+}
